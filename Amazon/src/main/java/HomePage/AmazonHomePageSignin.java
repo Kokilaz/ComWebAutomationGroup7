406e6eecec1a99f67481d4398a625a9981235eac
JenniferKasiStephen <jenniferkasi234@gmail.com>
2019-03-26 15:57:17 -0400

    moretestCases

---
+++ Amazon/src/main/java/HomePage/AmazonHomePageSignin.java
@@ -0,0 +1,54 @@
+package HomePage;
+
+import Base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AmazonHomePageSignin extends CommonAPI {
+
+    @FindBy(css = "#nav-link-accountList > span.nav-line-1")
+    WebElement login;
+
+    @FindBy(xpath = "//input[@id='ap_email']" )
+    WebElement email;
+
+    @FindBy(xpath = "//div[@class='a-section']//input[@id='continue']")
+    WebElement beforepassword;
+
+    @FindBy (xpath = "//input[@id='ap_password']")
+    WebElement password;
+
+    @FindBy (xpath = "//input[@id='signInSubmit']" )
+    WebElement siginSubmit;
+
+
+    public void openlogin(){
+        login.click();
+    }
+
+    public void giveEmail(){
+        email.sendKeys("lovely_jenny87@yahoo.com");
+    }
+
+    public void setbeforepassword(){
+        email.click();
+    }
+
+    public void typePassword(){
+        password.sendKeys("asdfghjkl1234567");
+    }
+
+    public void clickSigin(){
+        siginSubmit.click();
+    }
+
+    public void openpageLogin(){
+        openlogin();
+        giveEmail();
+        setbeforepassword();
+        typePassword();
+        clickSigin();
+
+    }
+
+}
---
+++ Amazon/src/test/java/TestHomePage/TestAmazonHomePageSignin.java
@@ -0,0 +1,32 @@
+package TestHomePage;
+
+import HomePage.AmazonHomePageSignin;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestAmazonHomePageSignin extends AmazonHomePageSignin {
+
+    AmazonHomePageSignin amazonHomePageSignin;
+    String Url = "https://www.amazon.com";
+
+    @BeforeMethod
+    public void initialize(){
+        driver.navigate().to(Url);
+        amazonHomePageSignin = PageFactory.initElements(driver,AmazonHomePageSignin.class);
+    }
+
+    @Test
+    public void setopenlogin(){
+        amazonHomePageSignin.openlogin();
+    }
+
+    @Test
+    public void setOpenPageLogin(){
+        amazonHomePageSignin.openpageLogin();
+    }
+
+
+
+
+}
